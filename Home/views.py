20b907465a176e3809841667075c967021773d2e
Partho Debnath <parthodebnath28@gmail.com>
2021-07-13 01:06:32 +0600

    implement 'add to cart' option

--- Home/views.py
+++ Home/views.py
@@ -1,3 +1,4 @@
+from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
 from django.views.generic import DetailView
@@ -12,11 +13,27 @@ class indexview(View):
     template_name = 'Home/index.html'
     context_object_name1 = 'AllOffers'
     context_object_name2 = 'ProductsInfo'
+    context_object_name3 = 'AddCartInfo'
 
     def get(self, request, *args, **kwargs):
         a = self.model1.objects.all()
         b = self.model2.objects.all()
-        return render(request, self.template_name, {self.context_object_name1 : a, self.context_object_name2 : b})
+        info = request.session.items()
+        AddCartInfo = {}
+        for (key, value) in info:
+            AddCartInfo[self.model2.objects.get(pk=int(key))] = value
+        return render(request, self.template_name, {self.context_object_name1 : a,
+        self.context_object_name2 : b, self.context_object_name3 : AddCartInfo})
+    
+
+    def post(self, request, *args, **kwargs):
+        key = request.POST['ID']
+        print('\n\nID', key, '\n\n')
+        if key in request.session :
+            request.session[key] += 1
+        else:
+            request.session[key] = 1
+        return HttpResponseRedirect('/')
 
 
 
